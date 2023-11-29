66a29276df3a60134866cfa07c7c43eb66396554
Yichun <59522685+xieyichun50@users.noreply.github.com>
2023-11-29 16:25:25 +0800

    Update 02var.sh

--- population/02var.sh
+++ population/02var.sh
@@ -8,6 +8,7 @@ refgenome=${dir_bam}/AoH_genomic.fa
 cat ${wd}/samples_n_reads_described.txt | while read id1 id2;
 do
 bcftools mpileup --threads 36 --max-depth 1000 --min-BQ 20 --min-MQ 20 --excl-flags UNMAP,SECONDARY,QCFAIL,DUP --min-ireads 5 -f ${refgenome} -o ${dir_var}/${id2}.bcf -Ob ${dir_bam}/${id2}.sorted.bam
+bcftools index ${dir_var}/${id2}.bcf
 echo "/mnt/content_176/yichun/fungi/aspergillus/202309aoryzae10/variant/${id2}.bcf" >> ${dir_var}/filelist.txt
 done
 
