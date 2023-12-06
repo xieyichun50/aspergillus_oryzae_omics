136a12916e21c51064095835de81696cdf6b9d03
Yichun Xie <59522685+xieyichun50@users.noreply.github.com>
2023-12-06 10:21:36 +0800

    Create 02var_gatk.sh

---
+++ population/02var_gatk.sh
@@ -0,0 +1,8 @@
+wd=/mnt/content_176/yichun/fungi/aspergillus/202309aoryzae10
+dir_fq=${wd}/fastq
+dir_bam=${wd}/alignment_bwa
+dir_var=${wd}/variant
+refgenome=${dir_bam}/AoH_genomic.fa
+
+conda activate gatk
+echo "GenomeAnalysisTK -T UnifiedGenotyper -glm BOTH -nt 36 -rf BadCigar --sample_ploidy 1 -R ${refgenome} -o ${dir_var}/aory.merge.vcf -I $(ls ${dir_bam}/*.sorted.bam | sed ':a;N;s/\n/ -I /g;ba;')"
