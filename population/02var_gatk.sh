44a6daef940e45f6cdf47be2a010e8442f1f22b6
Yichun Xie <59522685+xieyichun50@users.noreply.github.com>
2023-12-06 10:26:13 +0800

    Update 02var_gatk.sh

--- population/02var_gatk.sh
+++ population/02var_gatk.sh
@@ -4,5 +4,8 @@ dir_bam=${wd}/alignment_bwa
 dir_var=${wd}/variant
 refgenome=${dir_bam}/AoH_genomic.fa
 
+samtools faidx ${refgenome}
+picard CreateSequenceDictionary --REFERENCE ${refgenome} --OUTPUT ${dir_bam}/AoH_genomic.dict
 conda activate gatk
+
 echo "GenomeAnalysisTK -T UnifiedGenotyper -glm BOTH -nt 36 -rf BadCigar --sample_ploidy 1 -R ${refgenome} -o ${dir_var}/aory.merge.vcf -I $(ls ${dir_bam}/*.sorted.bam | sed ':a;N;s/\n/ -I /g;ba;')"
